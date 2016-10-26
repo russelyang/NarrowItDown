298277a6052464d3477dc4d2040f526fb1306aa5
RusselYang <russelyang@gmail.com>
2016-10-25 22:00:35 -0700

    remove not working

--- js/app.js
+++ js/app.js
@@ -4,30 +4,61 @@
 	angular.module('NarrowItDownApp', [])
 		.controller('NarrowItDownController', NarrowItDownController)
 		.service('NarrowItDownService', NarrowItDownService)
+		.directive('foundItems', FoundItemsDirective)
 		.constant('BASE_URL', 'https://davids-restaurant.herokuapp.com');
 
 	NarrowItDownController.$inject = ['$scope', 'NarrowItDownService'];
 
 	function NarrowItDownController($scope, narrowItDownService) {
-		$scope.narrowItDown = function() {
-			narrowItDownService.getMatchedMenuItems($scope.searchTerm);
+		var list = this;
+
+		list.foundItems = [];
+		list.narrowItDown = function() {
+			narrowItDownService.getMatchedMenuItems(list.searchTerm).then(function(foundItems)
+				{
+					list.foundItems = foundItems;
+				});
 		};
+
+		list.onRemove = function(index) {
+			list.foundItems.splice(index, 1);
+		}
 	}
 
-	NarrowItDownService.$inject = ["$http", "BASE_URL"];
-	function NarrowItDownService($http, BASE_URL) {
+
+	function FoundItemsDirective() {
+	  var ddo = {
+	    templateUrl: 'foundItems.html',
+	    scope: {
+	      items: '<',
+	      onRemove: '&'
+	    },
+	    controller: FoundItemsDirectiveController,
+	    controllerAs: 'list',
+	    bindToController: true
+	  };
+
+	  return ddo;
+	}
+
+	function FoundItemsDirectiveController() {
+
+
+	}
+
+
+	NarrowItDownService.$inject = ["$http", "BASE_URL", "$q"];
+	function NarrowItDownService($http, BASE_URL, $q) {
 		var service = this;
 
 		service.getMatchedMenuItems = function(searchTerm) {
 
-			$http.get(BASE_URL + '/menu_items.json').then(function(result) {
-				var promise = new Promise();
+			return $http.get(BASE_URL + '/menu_items.json').then(function(result) {
 				var items = result.data.menu_items;
 				var founds= items.filter(function(item) {
 					return item.description.indexOf(searchTerm) > -1;
 				});
-				promise.resolve(founds);
-				return promise;				
+				return $q.resolve(founds);
 			});
 		}
 	}
